3ae3b5c8d4d05b2668e836ce2da56674d1c4c147
mitchell <72942268+mitchellxz@users.noreply.github.com>
2024-02-16 21:45:13 -0500

    Delete mysite directory

--- mysite/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("index", views.index, name="index"),
-    path("", views.home, name="home"),
-    path("display_columns", views.display_columns, name='display_columns'),
-    path("results", views.results, name="results"),
-]
